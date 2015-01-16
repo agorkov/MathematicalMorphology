bbe9cb4e3f1857963f026b29b0dbd07e89b38987
Alexey <gorkoff@gmail.com>
2015-01-16 17:08:39 +0300

    Добавлена работа с маской

--- src/UFMain.dfm
+++ src/UFMain.dfm
@@ -3,7 +3,7 @@ object FMain: TFMain
   Top = 0
   Caption = #1054#1087#1077#1088#1072#1094#1080#1080' '#1084#1072#1090#1077#1084#1072#1090#1080#1095#1077#1089#1082#1086#1081' '#1084#1086#1088#1092#1086#1083#1086#1075#1080#1080'...'
   ClientHeight = 435
-  ClientWidth = 346
+  ClientWidth = 680
   Color = clBtnFace
   Font.Charset = DEFAULT_CHARSET
   Font.Color = clWindowText
@@ -24,6 +24,16 @@ object FMain: TFMain
     Proportional = True
     Stretch = True
     OnMouseDown = ImgOriginMouseDown
+  end
+  object ImgMask: TImage
+    Left = 344
+    Top = 93
+    Width = 330
+    Height = 330
+    Center = True
+    Proportional = True
+    Stretch = True
+    OnMouseDown = ImgMaskMouseDown
   end
   object GPImgSettings: TGroupBox
     Left = 8
@@ -46,7 +56,7 @@ object FMain: TFMain
       Height = 13
       Caption = '- '#1074#1099#1089#1086#1090#1072
     end
-    object EImgW: TEdit
+    object EIinW: TEdit
       Left = 17
       Top = 20
       Width = 32
@@ -55,19 +65,19 @@ object FMain: TFMain
       TabOrder = 0
       Text = '10'
     end
-    object UDImgW: TUpDown
+    object UDIInW: TUpDown
       Left = 49
       Top = 20
       Width = 16
       Height = 21
-      Associate = EImgW
+      Associate = EIinW
       Min = 2
       Max = 30
       Position = 10
       TabOrder = 1
-      OnClick = UDClick
+      OnClick = UDIInClick
     end
-    object EImgH: TEdit
+    object EIInH: TEdit
       Left = 17
       Top = 47
       Width = 32
@@ -76,17 +86,17 @@ object FMain: TFMain
       TabOrder = 2
       Text = '10'
     end
-    object UDImgH: TUpDown
+    object UDIInH: TUpDown
       Left = 49
       Top = 47
       Width = 16
       Height = 21
-      Associate = EImgH
+      Associate = EIInH
       Min = 2
       Max = 30
       Position = 10
       TabOrder = 3
-      OnClick = UDClick
+      OnClick = UDIInClick
     end
     object BSaveIIn: TButton
       Left = 152
@@ -96,6 +106,79 @@ object FMain: TFMain
       Caption = #1057#1086#1093#1088#1072#1085#1080#1090#1100
       TabOrder = 4
       OnClick = BSaveIInClick
+    end
+  end
+  object GroupBox1: TGroupBox
+    Left = 344
+    Top = 6
+    Width = 330
+    Height = 81
+    Caption = #1053#1072#1089#1090#1088#1086#1081#1082#1080' '#1080#1079#1086#1073#1088#1072#1078#1077#1085#1080#1103':'
+    TabOrder = 1
+    object Label3: TLabel
+      Left = 71
+      Top = 20
+      Width = 45
+      Height = 13
+      Caption = '- '#1096#1080#1088#1080#1085#1072
+    end
+    object Label4: TLabel
+      Left = 71
+      Top = 47
+      Width = 44
+      Height = 13
+      Caption = '- '#1074#1099#1089#1086#1090#1072
+    end
+    object EMaskW: TEdit
+      Left = 17
+      Top = 20
+      Width = 32
+      Height = 21
+      ReadOnly = True
+      TabOrder = 0
+      Text = '3'
+    end
+    object UDMaskW: TUpDown
+      Left = 49
+      Top = 20
+      Width = 15
+      Height = 21
+      Associate = EMaskW
+      Min = 2
+      Max = 10
+      Position = 3
+      TabOrder = 1
+      OnClick = UDMaskClick
+    end
+    object EMaskH: TEdit
+      Left = 17
+      Top = 47
+      Width = 32
+      Height = 21
+      ReadOnly = True
+      TabOrder = 2
+      Text = '3'
+    end
+    object UDMaskH: TUpDown
+      Left = 49
+      Top = 47
+      Width = 15
+      Height = 21
+      Associate = EMaskH
+      Min = 2
+      Max = 10
+      Position = 3
+      TabOrder = 3
+      OnClick = UDMaskClick
+    end
+    object Button1: TButton
+      Left = 152
+      Top = 15
+      Width = 163
+      Height = 53
+      Caption = #1057#1086#1093#1088#1072#1085#1080#1090#1100
+      TabOrder = 4
+      OnClick = Button1Click
     end
   end
   object SPD: TSavePictureDialog
--- src/UFMain.pas
+++ src/UFMain.pas
@@ -11,17 +11,30 @@ type
     GPImgSettings: TGroupBox;
     Label1: TLabel;
     Label2: TLabel;
-    EImgW: TEdit;
-    UDImgW: TUpDown;
-    EImgH: TEdit;
-    UDImgH: TUpDown;
+    EIinW: TEdit;
+    UDIInW: TUpDown;
+    EIInH: TEdit;
+    UDIInH: TUpDown;
     ImgOrigin: TImage;
     SPD: TSavePictureDialog;
     BSaveIIn: TButton;
-    procedure UDClick(Sender: TObject; Button: TUDBtnType);
+    ImgMask: TImage;
+    GroupBox1: TGroupBox;
+    Label3: TLabel;
+    Label4: TLabel;
+    EMaskW: TEdit;
+    UDMaskW: TUpDown;
+    EMaskH: TEdit;
+    UDMaskH: TUpDown;
+    Button1: TButton;
+    procedure UDIInClick(Sender: TObject; Button: TUDBtnType);
+    procedure UDMaskClick(Sender: TObject; Button: TUDBtnType);
     procedure FormCreate(Sender: TObject);
     procedure ImgOriginMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
     procedure BSaveIInClick(Sender: TObject);
+    procedure ImgMaskMouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure Button1Click(Sender: TObject);
   private
 
   public
@@ -30,6 +43,7 @@ type
     procedure DrawIOut;
     procedure DrawAll;
     procedure ResizeBI;
+    procedure ResizeMask;
   end;
 
 var
@@ -44,7 +58,7 @@ uses
   UBinaryImages, UGrayscaleImages, UFBigImage, Math, UBitMapFunctions;
 
 var
-  IIn: TCBinaryImage;
+  IIn, Mask: TCBinaryImage;
 
 procedure TFMain.DrawIIn;
 var
@@ -58,7 +72,7 @@ begin
   ImgOrigin.Canvas.Brush.Color := clGray;
   ImgOrigin.Canvas.Rectangle(0, 0, ImgOrigin.Height - 1, ImgOrigin.Width - 1);
   /// Выбираем размер пикселя
-  w := min(ImgOrigin.Height div IIn.Height, ImgOrigin.Height div IIn.Width);
+  w := min(ImgOrigin.Height div IIn.Height, ImgOrigin.Width div IIn.Width);
   /// Отрисовываем изображение
   for i := 1 to IIn.Height do
     for j := 1 to IIn.Width do
@@ -71,34 +85,98 @@ begin
     end;
 end;
 
+procedure TFMain.DrawMask;
+var
+  i, j: word;
+  w: word;
+begin
+  /// Очищаем изображение
+  ImgMask.Picture.Bitmap.Height := ImgMask.Height;
+  ImgMask.Picture.Bitmap.Width := ImgMask.Width;
+  ImgMask.Canvas.Pen.Color := clGray;
+  ImgMask.Canvas.Brush.Color := clGray;
+  ImgMask.Canvas.Rectangle(0, 0, ImgMask.Height - 1, ImgMask.Width - 1);
+  /// Выбираем размер пикселя
+  w := min(ImgMask.Height div Mask.Height, ImgMask.Width div Mask.Width);
+  /// Отрисовываем изображение
+  for i := 1 to Mask.Height do
+    for j := 1 to Mask.Width do
+    begin
+      if Mask.Pixels[i - 1, j - 1] then
+        ImgMask.Canvas.Brush.Color := clBlack
+      else
+        ImgMask.Canvas.Brush.Color := clWhite;
+      ImgMask.Canvas.Rectangle((j - 1) * w, (i - 1) * w, j * w, i * w);
+    end;
+end;
+
+procedure TFMain.DrawIOut;
+begin
+
+end;
+
+procedure TFMain.Button1Click(Sender: TObject);
+var
+  FileName: string;
+  BM: TBitMap;
+begin
+  if SPD.Execute then
+  begin
+    FileName := SPD.FileName;
+    if UpperCase(ExtractFileExt(FileName)) <> '.PNG' then
+      FileName := FileName + '.PNG';
+    BM := Mask.SaveToBitMap;
+    UBitMapFunctions.SaveToFile(BM, FileName);
+    BM.Free;
+  end;
+end;
+
+procedure TFMain.DrawAll;
+begin
+  DrawIIn;
+  DrawMask;
+  DrawIOut;
+end;
+
+procedure TFMain.ImgMaskMouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+var
+  w: word;
+begin
+  /// Выбираем размер пикселя
+  w := min(ImgMask.Height div Mask.Height, ImgMask.Width div Mask.Width);
+  /// Меняем значение соответствующего пиксела на противоположное
+  Mask.Pixels[Y div w, X div w] := not Mask.Pixels[Y div w, X div w];
+  /// Отрисовываем исходное изображение
+  DrawMask;
+end;
+
 procedure TFMain.ImgOriginMouseDown(Sender: TObject; Button: TMouseButton; Shift: TShiftState; X, Y: Integer);
 var
   w: word;
 begin
   /// Выбираем размер пикселя
-  w := min(ImgOrigin.Height div IIn.Height, ImgOrigin.Height div IIn.Width);
+  w := min(ImgOrigin.Height div IIn.Height, ImgOrigin.Width div IIn.Width);
   /// Меняем значение соответствующего пиксела на противоположное
   IIn.Pixels[Y div w, X div w] := not IIn.Pixels[Y div w, X div w];
   /// Отрисовываем исходное изображение
   DrawIIn;
 end;
 
-procedure TFMain.DrawMask;
-begin
-
-end;
-
 procedure TFMain.FormCreate(Sender: TObject);
+var
+  i, j: word;
 begin
   IIn := TCBinaryImage.Create;
   IIn.Height := 10;
   IIn.Width := 10;
+  Mask := TCBinaryImage.Create;
+  Mask.Height := 3;
+  Mask.Width := 3;
+  for i := 0 to Mask.Height - 1 do
+    for j := 0 to Mask.Width - 1 do
+      Mask.Pixels[i, j] := true;
   DrawAll;
-end;
-
-procedure TFMain.DrawIOut;
-begin
-
 end;
 
 procedure TFMain.BSaveIInClick(Sender: TObject);
@@ -117,23 +195,28 @@ begin
   end;
 end;
 
-procedure TFMain.DrawAll;
-begin
-  DrawIIn;
-  DrawMask;
-  DrawIOut;
-end;
-
 procedure TFMain.ResizeBI;
 begin
-  IIn.Height := UDImgH.Position;
-  IIn.Width := UDImgW.Position;
+  IIn.Height := UDIInH.Position;
+  IIn.Width := UDIInW.Position;
   DrawAll;
 end;
 
-procedure TFMain.UDClick(Sender: TObject; Button: TUDBtnType);
+procedure TFMain.ResizeMask;
+begin
+  Mask.Height := UDMaskH.Position;
+  Mask.Width := UDMaskW.Position;
+  DrawAll;
+end;
+
+procedure TFMain.UDIInClick(Sender: TObject; Button: TUDBtnType);
 begin
   ResizeBI;
+end;
+
+procedure TFMain.UDMaskClick(Sender: TObject; Button: TUDBtnType);
+begin
+  ResizeMask;
 end;
 
 end.
